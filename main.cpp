3cb1ca5f6d52115f7e1eeff314974cd289bc3b01
Jason Martinez <jason1994.8@gmail.com>
2015-06-07 12:58:23 -0500

    Version con Box

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include "Box.hpp"
 
 int main()
 {
 	std::cout << "Hello world" << std::endl;
+
+	Box b1(30, 50);
+
+	std::cout << "Width = " << b1.getWidth() << std::endl;
+	std::cout << "Height = " << b1.getHeight() << std::endl;
+	std::cout << "Area = " << b1.area() << std::endl;
 	return 0;
 }
